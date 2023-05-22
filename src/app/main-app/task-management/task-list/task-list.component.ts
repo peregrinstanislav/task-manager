3d287bcaf6439ee5bec6f4d47805424f1e35e25d
Stanislav Peregrin <stanislav.peregrin@datapac.sk>
2023-05-22 11:10:23 +0200

    improvements

--- src/app/main-app/task-management/task-list/task-list.component.ts
+++ src/app/main-app/task-management/task-list/task-list.component.ts
@@ -93,7 +93,6 @@ export class TaskListComponent implements OnInit {
     onAddNewTaskClick(): void {
         this.dialog.open(TaskChoserComponent, {
             autoFocus: true,
-            disableClose: true,
             width: '300px'
         }).afterClosed().pipe(take(1)).subscribe(result => {
             if (result?.selectedType) {
